83605b3d93d3148df489b6b3093ab1c1cbcb8f9e
Vadász Péter <peter.vadasz1995@gmail.com>
2018-03-23 17:43:40 +0100

    FalsyQuiz-4: Create action listeners.

--- app/src/main/java/hu/falsyquiz/falsyquiz/Activities/MainMenuActivity.java
+++ app/src/main/java/hu/falsyquiz/falsyquiz/Activities/MainMenuActivity.java
@@ -1,15 +1,36 @@
 package hu.falsyquiz.falsyquiz.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import hu.falsyquiz.falsyquiz.R;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    @BindView(R.id.mainMenuActivity_startButton)
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ButterKnife.bind(this);
+
+        initOnClickListeners();
+    }
+
+    private void initOnClickListeners() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, QuestionActivity.class));
+            }
+        });
     }
 }
