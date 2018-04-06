00bb1dc007c0f5224cf23bcc9737b212ba017ded
Vadász Péter <peter.vadasz1995@gmail.com>
2018-04-06 19:20:03 +0200

    FalsyQuiz-15: Fix question loading.

--- app/src/main/java/hu/falsyquiz/falsyquiz/Activities/QuestionActivity.java
+++ app/src/main/java/hu/falsyquiz/falsyquiz/Activities/QuestionActivity.java
@@ -1,10 +1,7 @@
 package hu.falsyquiz.falsyquiz.Activities;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
