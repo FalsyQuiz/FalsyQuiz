86d9a6d6678053d54d52a9d990c05fb6b511434a
Kiss David <deco1996@mailbox.hu>
2018-04-25 11:05:36 +0200

    Vibration action, added some sound files

--- app/src/main/java/hu/falsyquiz/falsyquiz/Actions/Actions.java
+++ app/src/main/java/hu/falsyquiz/falsyquiz/Actions/Actions.java
@@ -1,15 +1,21 @@
 package hu.falsyquiz.falsyquiz.Actions;
 
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Vibrator;
+
 /**
  * Created by dave on 2018.04.24..
  */
 
 public class Actions {
-    public void vibrate(int milliseconds) {
-        //to be implemented...
+    public static void vibrate(Activity activity, int milliseconds) {
+        Vibrator v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(milliseconds);
     }
 
-    public void playRandomSound() {
+    public static void playRandomSound() {
         //to be implemented...
     }
 }
