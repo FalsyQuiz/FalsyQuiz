e31c6a7b1d84fecf08822a7db2bd73412cf20617
Gecse Viktoria <viktoriagecse3@gmail.com>
2018-04-25 15:45:58 +0200

    FalsyQuiz-61: Changed GameOver activity's resultPictures and designed GameOver activity's text + mainpage's text

--- app/src/main/java/hu/falsyquiz/falsyquiz/Activities/GameOverActivity.java
+++ app/src/main/java/hu/falsyquiz/falsyquiz/Activities/GameOverActivity.java
@@ -2,6 +2,7 @@ package hu.falsyquiz.falsyquiz.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +38,8 @@ public class GameOverActivity extends AbstractActivity {
 
         Intent intent = getIntent();
         Game game = intent.getParcelableExtra(EXTRA_GAMER_KEY);
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(getResources().getColor(R.color.white));
 
         if (game.gameOver()) {
             fail(game);
@@ -55,16 +58,22 @@ public class GameOverActivity extends AbstractActivity {
 
     private void fail(Game game) {
         resultText.setText(getString(R.string.gameOverActivity_failed_text));
+        resultText.setTextColor(getResources().getColor(R.color.red));
         resultPicture.setImageResource(R.mipmap.failed);
         songPlayer = new SongPlayer(this, R.raw.laugh);
         songPlayer.playSong();
+        percentageText.setTextColor(getResources().getColor(R.color.colorPrimary));
+        numOfQuestionsText.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
         printResults(game);
     }
 
     private void win(Game game) {
         resultText.setText(getString(R.string.gameOverActivity_win_text));
+        resultText.setTextColor(getResources().getColor(R.color.red));
         songPlayer = new SongPlayer(this, R.raw.human_audience_cheer_and_clap);
         songPlayer.playSong();
+        percentageText.setTextColor(getResources().getColor(R.color.colorPrimary));
+        numOfQuestionsText.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
         resultPicture.setImageResource(R.mipmap.winner);
         printResults(game);
     }
