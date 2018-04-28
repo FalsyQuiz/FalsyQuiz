948b7202061b55488be98496551a88ecd4dc6f06
Vadász Péter <peter.vadasz1995@gmail.com>
2018-04-28 19:06:13 +0200

    FalsyQuiz: Configure and automatize javadoc.

--- auto_deploy_win.ps1
+++ auto_deploy_win.ps1
@@ -1,3 +1,4 @@
 .\gradlew lint
+.\gradlew javadoc
 .\gradlew assembleDebug
 pscp -i key.ppk app\build\outputs\apk\debug\app-debug.apk c4367falsyQuiz@wh04.rackhost.hu:../../web/vadaszfoto.hu/FalsyQuiz
